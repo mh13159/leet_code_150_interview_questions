93cc0b2c73331546e0349372cbb32339355aa94a
mh13159 <m.hamza@vitalpakistantrust.org>
2024-11-12 11:21:09 +0500

    final_solution

--- merge_two_non_decreasing_lists.py
+++ merge_two_non_decreasing_lists.py
@@ -1,34 +1,36 @@
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        # Set up pointers for nums1 and nums2 starting from the end
+        p1, p2, p = m - 1, n - 1, m + n - 1
 
-nums1 = [1,2,3,0,0,0]
-nums2 = [2,5,6]
+        # Traverse nums1 and nums2 from the end to the beginning
+        while p1 >= 0 and p2 >= 0:
+            if nums1[p1] > nums2[p2]:
+                nums1[p] = nums1[p1]
+                p1 -= 1
+            else:
+                nums1[p] = nums2[p2]
+                p2 -= 1
+            p -= 1
 
-m=3
-n=3
+        # If there are any remaining elements in nums2, copy them
+        if p2 >= 0:
+            nums1[:p2 + 1] = nums2[:p2 + 1]
 
-if nums1[0]==0:
-    nums1 = nums2[:]
-    print(nums1)
-    exit()
-nums2 = nums2[::-1]
-for element in nums2:
-    #print(element)
-    nums1_non_zero= nums1[0:m]
-    #print(nums1_non_zero)
-    nums1_last_element = -1
-    nums1_max = nums1_non_zero[nums1_last_element]
-    #print(nums1_max)
-    while element< nums1_max:
-        #print(element)
-        #print(nums1_max)
-        nums1_last_element= nums1_last_element-1
-        nums1_max = nums1_non_zero[nums1_last_element]
-        #print(nums1_max)
-        #print(nums1_last_element)
-    if nums1[m+n+nums1_last_element]==0:
-        nums1[m+n+nums1_last_element]=element
-    else:
-        print(nums1[m+n+nums1_last_element])
-        nums1[m+n+nums1_last_element+1]=nums1[m+n+nums1_last_element]
-        nums1[m+n+nums1_last_element]=element
-    n-=1
-    print(nums1)
+# Instantiate the Solution class
+solution = Solution()
+
+# Define multiple test cases
+test_cases = [
+    {"nums1": [1, 2, 3, 0, 0, 0], "m": 3, "nums2": [2, 5, 6], "n": 3},
+    {"nums1": [1], "m": 1, "nums2": [], "n": 0},
+    {"nums1": [0], "m": 0, "nums2": [1], "n": 1},
+    {"nums1": [4, 5, 6, 0, 0, 0], "m": 3, "nums2": [1, 2, 3], "n": 3},
+    {"nums1": [2, 0], "m": 1, "nums2": [1], "n": 1},
+]
+
+# Execute each test case
+for i, case in enumerate(test_cases):
+    nums1, m, nums2, n = case["nums1"], case["m"], case["nums2"], case["n"]
+    solution.merge(nums1, m, nums2, n)
+    print(f"Test case {i+1}: {nums1}")
